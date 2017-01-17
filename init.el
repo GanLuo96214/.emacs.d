e380a19c3b2e25f2703cd9c0b96229fc64b5fa95
ganluo960214 <qq47106040>
2017-01-17 15:39:33 +0800

    add init.el

---
+++ init.el
@@ -0,0 +1,20 @@
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-linum-mode 1)
+
+(setq
+ make-backup-files nil;
+ inhibit-splash-screen 1;
+ )
+
+(set-face-attribute
+ 'default nil :height 160
+ )
+
+(fset
+ 'yes-or-no-p 'y-or-n-p;
+ )
+
+;(global-set-key
+;
+; )
